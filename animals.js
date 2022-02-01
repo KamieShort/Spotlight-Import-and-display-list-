3227e8e437c4ae0034688b10aa0cd59ebcb9b6fd
Kamie Short <kamie.short@gmail.com>
2022-02-01 09:19:56 -0800

    setting up data

---
+++ animals.js
@@ -0,0 +1,32 @@
+export const animals = [
+    {
+    type: 'dog',
+    numberOfLegs: 4,
+    class: 'mammals',
+    cuteness: 10,
+    image: 
+}
+
+{
+    type: 'pig',
+    numberOfLegs: 4,
+    class: 'mammals',
+    cuteness: 12,
+    image: 
+}
+
+{
+    type: 'orca',
+    numberOfLegs: 4,
+    class: 'mammals',
+    cuteness: 2,
+    image: 
+}
+
+{
+    type: 'wombat',
+    numberOfLegs: 2,
+    class: 'mammals',
+    cuteness: 10,
+    image: 
+}]
